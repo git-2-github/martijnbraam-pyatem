b13d6caa4730e8a1c3b83e49d9eaf3d7df6dfc01
Martijn Braam <martijn@brixit.nl>
2021-12-03 22:47:43 +0100

    Add unit test for atem_to_rgb

--- pyatem/test_media.py
+++ pyatem/test_media.py
@@ -1,18 +1,23 @@
 from unittest import TestCase
 
 from pyatem.media import rle_decode
+from pyatem.mediaconvert import atem_to_rgb
 
 
 class Test(TestCase):
     FRAME_1080_RED = b'\xfe\xfe\xfe\xfe\xfe\xfe\xfe\xfe\x00\x00\x00\x00\x00\x0f\xd2\x00:\x96d\xfa:\x9e\xfc\xfa'
     FRAME_1080_RED_PIXEL = b':\x96d\xfa:\x9e\xfc\xfa'
+    RED_PIXEL_8888 = b'\xff\0\0\xff'
 
     def test_atem_to_image(self):
-        YCbCrA10Bit422 = rle_decode(self.FRAME_1080_RED)
-
+        decompressed = rle_decode(self.FRAME_1080_RED)
+        result = atem_to_rgb(decompressed, 1920, 1080)
+        for index in range(0, len(result) - 8, 4):
+            pixel = result[index:index + 4]
+            self.assertEqual(self.RED_PIXEL_8888, pixel, msg='index {}'.format(index))
 
     def test_rle_decode_solid_color(self):
         result = rle_decode(self.FRAME_1080_RED)
         for index in range(0, len(result) - 8, 8):
             pixel = result[index:index + 8]
-            self.assertEqual(pixel, self.FRAME_1080_RED_PIXEL)
+            self.assertEqual(self.FRAME_1080_RED_PIXEL, pixel)
