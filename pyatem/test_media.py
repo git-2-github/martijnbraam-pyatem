40f421497a14cc82170710cd5d2124ab3e03141f
Martijn Braam <martijn@brixit.nl>
2021-12-03 23:34:40 +0100

    Add RLE encoder
    The encoder is not optimal yet since it outputs 8 bytes more than the closed source implementation if the first byte is also the same as the rest.

--- pyatem/test_media.py
+++ pyatem/test_media.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from pyatem.media import rle_decode
+from pyatem.media import rle_decode, rle_encode
 from pyatem.mediaconvert import atem_to_rgb
 
 
@@ -21,3 +21,9 @@ class Test(TestCase):
         for index in range(0, len(result) - 8, 8):
             pixel = result[index:index + 8]
             self.assertEqual(self.FRAME_1080_RED_PIXEL, pixel)
+
+    def test_rle_encode_solid_color(self):
+        testframe = rle_decode(self.FRAME_1080_RED)
+        compressed = rle_encode(testframe)
+        decompressed = rle_decode(compressed)
+        self.assertEqual(testframe, decompressed)
