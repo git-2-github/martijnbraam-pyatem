4e12ae6ad53270ee80e302382fdaa35445daa3d4
Martijn Braam <martijn@brixit.nl>
2021-12-04 13:52:38 +0100

    Use C implementation for the RLE encoder

--- pyatem/mediaconvertmodule.c
+++ pyatem/mediaconvertmodule.c
@@ -1,11 +1,12 @@
 #define PY_SSIZE_T_CLEAN
+
 #include <Python.h>
 
 const double bt709_coeff_r = 0.2126;
 const double bt709_coeff_g = 0.7152;
 const double bt709_coeff_b = 0.0722;
-const double bt709_coeff_ri = 1.0-bt709_coeff_r;
-const double bt709_coeff_bi = 1.0-bt709_coeff_b;
+const double bt709_coeff_ri = 1.0 - bt709_coeff_r;
+const double bt709_coeff_bi = 1.0 - bt709_coeff_b;
 const double bt709_coeff_bg = bt709_coeff_b / bt709_coeff_g;
 const double bt709_coeff_rg = bt709_coeff_r / bt709_coeff_g;
 
@@ -13,31 +14,38 @@ const int y_offset = 16 << 8;
 const int y_range = 219;
 const int cr_offset = 128 << 8;
 const int cr_range = 224;
-const int cr_middle = 224/2;
+const int cr_middle = 224 / 2;
 
 const double bt709_ri_range = bt709_coeff_ri / cr_middle;
 const double bt709_bi_range = bt709_coeff_bi / cr_middle;
 
 
-static PyObject *method_atem_to_rgb(PyObject *self, PyObject *args) {
+static PyObject *
+method_atem_to_rgb(PyObject *self, PyObject *args)
+{
     Py_buffer input_buffer;
     Py_ssize_t data_length;
     unsigned int width, height;
     PyObject *res;
 
     /* Parse arguments */
-    if(!PyArg_ParseTuple(args, "y*II", &input_buffer, &width, &height)) {
+    if (!PyArg_ParseTuple(args, "y*II", &input_buffer, &width, &height)) {
         return NULL;
     }
 
     data_length = input_buffer.len;
     char *buffer;
     char *resbuffer = (char *) malloc(data_length);
+
+    if (resbuffer == NULL) {
+        return PyErr_NoMemory();
+    }
+
     char *outbuffer = resbuffer;
 
     int pixel_size = 8;
     buffer = input_buffer.buf;
-    for(int i=0; i<data_length; i += pixel_size ){
+    for (int i = 0; i < data_length; i += pixel_size) {
         buffer += pixel_size;
 
         // Convert 10-bit BT.709 Y'CbCrA 4:2:2 to RGB
@@ -49,10 +57,10 @@ static PyObject *method_atem_to_rgb(PyObject *self, PyObject *args) {
         unsigned short y1 = ((buffer[2] & 0x03) << 8) + (buffer[3] & 0xff);
         unsigned short y2 = ((buffer[6] & 0x03) << 8) + (buffer[7] & 0xff);
 
-        float cbf = bt709_bi_range * ((cb<<6) - cr_offset);
-        float crf = bt709_ri_range * ((cr<<6) - cr_offset);
-        float y1f = ((double)(y1 << 6)-y_offset) / y_range;
-        float y2f = ((double)(y2 << 6)-y_offset) / y_range;
+        float cbf = bt709_bi_range * ((cb << 6) - cr_offset);
+        float crf = bt709_ri_range * ((cr << 6) - cr_offset);
+        float y1f = ((double) (y1 << 6) - y_offset) / y_range;
+        float y2f = ((double) (y2 << 6) - y_offset) / y_range;
 
         float r1f = fmin(255, y1f + crf);
         float g1f = fmin(255, y1f - cbf * bt709_coeff_bg - crf * bt709_coeff_rg);
@@ -61,14 +69,14 @@ static PyObject *method_atem_to_rgb(PyObject *self, PyObject *args) {
         float g2f = fmin(255, y2f - cbf * bt709_coeff_bg - crf * bt709_coeff_rg);
         float b2f = fmin(255, y2f + cbf);
 
-        outbuffer[0] = (unsigned char)r1f;
-        outbuffer[1] = (unsigned char)g1f;
-        outbuffer[2] = (unsigned char)b1f;
-        outbuffer[3] = (unsigned char)(((double)(a1 -16)) / 3.6);
-        outbuffer[4] = (unsigned char)r2f;
-        outbuffer[5] = (unsigned char)g2f;
-        outbuffer[6] = (unsigned char)b2f;
-        outbuffer[7] = (unsigned char)(((double)(a2 -16)) / 3.6);
+        outbuffer[0] = (unsigned char) r1f;
+        outbuffer[1] = (unsigned char) g1f;
+        outbuffer[2] = (unsigned char) b1f;
+        outbuffer[3] = (unsigned char) (((double) (a1 - 16)) / 3.6);
+        outbuffer[4] = (unsigned char) r2f;
+        outbuffer[5] = (unsigned char) g2f;
+        outbuffer[6] = (unsigned char) b2f;
+        outbuffer[7] = (unsigned char) (((double) (a2 - 16)) / 3.6);
         outbuffer += pixel_size;
     }
 
@@ -77,9 +85,89 @@ static PyObject *method_atem_to_rgb(PyObject *self, PyObject *args) {
     return res;
 }
 
+
+static PyObject *
+method_rle_encode(PyObject *self, PyObject *args)
+{
+    Py_buffer input_buffer;
+    PyObject *res;
+
+    /* Parse arguments */
+    if (!PyArg_ParseTuple(args, "y*", &input_buffer)) {
+        return NULL;
+    }
+
+    char *data;
+    data = input_buffer.buf;
+
+    unsigned char *buffer = (unsigned char *) malloc(input_buffer.len);
+
+    if (buffer == NULL) {
+        return PyErr_NoMemory();
+    }
+
+    unsigned long long block_counter = 0;
+    ssize_t lastblock = -1;
+    ssize_t wp = 0;
+    for (int i = 0; i < input_buffer.len; i += 8) {
+        if (memcmp(&data[i], &data[lastblock], 8) == 0) {
+            // Block is same as previous, increment counter only
+            block_counter++;
+            continue;
+        }
+        if (block_counter > 2) {
+            // RLE header
+            memset(&buffer[wp], 0xfe, 8);
+            wp += 8;
+
+            // Block repeat count
+            for (int h = 0; h < 8; h++) buffer[wp + h] = (block_counter >> ((7 - h) * 8)) & 0xff;
+            wp += 8;
+
+            // Block value
+            memcpy(&buffer[wp], &data[lastblock], 8);
+            wp += 8;
+        }
+        if (block_counter > 0) {
+            // Only two repeats, the RLE header would make the compressed chunk longer
+            // write the 2 blocks without compression instead
+            for (unsigned long long j = 0; j < block_counter; j++) {
+                memcpy(&buffer[wp], &data[lastblock], 8);
+                wp += 8;
+            }
+        }
+
+        // Write the current block to the result
+        memcpy(&buffer[wp], &data[i], 8);
+        wp += 8;
+
+        // Reset for next iteration
+        lastblock = i;
+        block_counter = 0;
+    }
+    if (block_counter > 0) {
+        // RLE header
+        memset(&buffer[wp], 0xfe, 8);
+        wp += 8;
+
+        // Block repeat count
+        for (int h = 0; h < 8; h++) buffer[wp + h] = (block_counter >> ((7 - h) * 8)) & 0xff;
+        wp += 8;
+
+        // Block value
+        memcpy(&buffer[wp], &data[lastblock], 8);
+        wp += 8;
+    }
+
+    res = Py_BuildValue("y#", buffer, wp);
+    free(buffer);
+    return res;
+}
+
 static PyMethodDef MediaConvertMethods[] = {
     {"atem_to_rgb", method_atem_to_rgb, METH_VARARGS, "Convert an Atem YCbCrA frame to RGB8888"},
-    {NULL, NULL, 0, NULL},
+    {"rle_encode",  method_rle_encode,  METH_VARARGS, "Compress data using the custom Atem RLE encoding"},
+    {NULL,          NULL,               0,            NULL},
 };
 
 static struct PyModuleDef mediaconvertmodule = {
@@ -90,6 +178,8 @@ static struct PyModuleDef mediaconvertmodule = {
     MediaConvertMethods,
 };
 
-PyMODINIT_FUNC PyInit_mediaconvert(void){
+PyMODINIT_FUNC
+PyInit_mediaconvert(void)
+{
     return PyModule_Create(&mediaconvertmodule);
 }
