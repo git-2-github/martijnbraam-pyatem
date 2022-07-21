e22543d0d875abce7cb9f1904e1442aaffd58c05
Martijn Braam <martijn@brixit.nl>
2022-07-21 12:55:35 +0200

    pyatem: initial converter protocol implementation
    This implements the USB protocol used by BMD Converter Setup
    and is tested on an BMD BiDi 12G converter

---
+++ pyatem/converters/__main__.py
@@ -0,0 +1,57 @@
+import argparse
+import pyatem.converters.converter as conv
+
+
+def enumerate_hardware():
+    classes = []
+    for name, cls in conv.__dict__.items():
+        if isinstance(cls, type) and name not in ['Field', 'Converter']:
+            classes.append(cls)
+    return classes
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Blackmagic Design Converter Setup")
+    args = parser.parse_args()
+
+    existing = []
+    for device in enumerate_hardware():
+        if device.is_plugged_in():
+            existing.append(device)
+
+    if len(existing) == 0:
+        print("No supported hardware detected")
+        exit(1)
+    elif len(existing) > 1:
+        print("Multiple converters found, not handled yet")
+        for dev in existing:
+            print(f" - {dev.NAME}")
+        exit(1)
+
+    deviceclass = existing[0]
+
+    print(f"Product:  {deviceclass.NAME}")
+
+    device = deviceclass()
+    device.connect()
+    last_section = None
+    for field in device.get_state().values():
+        if field.section != last_section:
+            print(f"\n===[ {field.section} ]===")
+            last_section = field.section
+        if field.dtype == str:
+            value = field.value.decode()
+        if field.dtype == int:
+            value = int.from_bytes(field.value, byteorder='little')
+
+        if field.mapping is None:
+            print(f'{field.label}:  {value}')
+        else:
+            print(field.label + ':')
+            for key, display in field.mapping.items():
+                x = 'x' if key == value else ' '
+                print(f'    [{x}] {display}')
+
+
+if __name__ == '__main__':
+    main()
---
+++ pyatem/converters/converter.py
@@ -0,0 +1,120 @@
+import usb.core
+import usb.util
+import struct
+
+
+class Field:
+    def __init__(self, name, dtype, section, label, mapping=None, sys=False):
+        self.name = name
+        self.section = section
+        self.dtype = dtype
+        self.label = label
+        self.sys = sys
+        self.mapping = mapping
+
+        self.value = None
+
+    def __repr__(self):
+        return f'<Field {self.name} ({self.label})>'
+
+
+class Converter:
+    VENDOR = 0x1edb
+    PRODUCT = 0
+    NAME = "Unknown"
+    FIELDS = []
+
+    def __init__(self):
+        self.handle = None
+
+    @classmethod
+    def is_plugged_in(cls):
+        result = usb.core.find(idVendor=cls.VENDOR, idProduct=cls.PRODUCT)
+        return result is not None
+
+    def connect(self):
+        self.handle = usb.core.find(idVendor=self.VENDOR, idProduct=self.PRODUCT)
+        self.handle.set_configuration(1)
+
+    def get_field(self, name, sys=False, write=None):
+        ep_read = 0xa1 if sys else 0xc0
+        ep_write = 0x21 if sys else 0x40
+        req_ticket = 1 if sys else 10
+        req_name = 2 if sys else 11
+        req_read = 3 if sys else 12
+        req_write = 4 if sys else 13
+
+        ticket = self.handle.ctrl_transfer(bmRequestType=ep_read,
+                                           bRequest=req_ticket,
+                                           wValue=0,
+                                           wIndex=0,
+                                           data_or_wLength=2)
+
+        ticket, = struct.unpack('<H', bytes(ticket))
+
+        self.handle.ctrl_transfer(bmRequestType=ep_write,
+                                  bRequest=req_name,
+                                  wValue=ticket,
+                                  wIndex=0,
+                                  data_or_wLength=name.encode())
+
+        if write is not None:
+            self.handle.ctrl_transfer(bmRequestType=ep_write,
+                                      bRequest=req_write,
+                                      wValue=ticket,
+                                      wIndex=0,
+                                      data_or_wLength=write)
+        else:
+            return bytes(self.handle.ctrl_transfer(bmRequestType=ep_read,
+                                                   bRequest=req_read,
+                                                   wValue=ticket,
+                                                   wIndex=0,
+                                                   data_or_wLength=255))
+
+    def set_field(self, name, value, sys=False):
+        return self.get_field(name, sys=sys, write=value)
+
+    def get_state(self):
+        result = {}
+        for field in self.FIELDS:
+            ret = self.get_field(field.name, sys=field.sys)
+            field.value = ret
+            result[field.name] = field
+        return result
+
+
+class MicroConverterBiDirectional12G(Converter):
+    PRODUCT = 0xbe89
+    NAME = "Blackmagic design Micro Converter BiDirectional SDI/HDMI 12G"
+
+    FIELDS = [
+        Field('DeviceName', str, 'Device', 'Name', sys=True),
+        Field('BuildId', str, 'Device', 'Build ID', sys=True),
+        Field('ReleaseVersion', str, 'Device', 'Software', sys=True),
+        Field('AtemId', int, 'SDI Camera Control', 'ATEM Camera ID'),
+        Field('SdiLevelAEnable', int, 'SDI Output', '3G SDI Output', mapping={
+            0xff: 'Level A',
+            0x00: 'Level B',
+        }),
+        Field('HdmiClampEnable', int, 'HDMI Output', 'Clip signal to', mapping={
+            0x00: 'Normal levels (16 - 235)',
+            0xff: 'Illegal levels (0 - 255)',
+        }),
+        Field('HdmiTxCh34Swap', int, 'HDMI Audio', 'For 5.1 surround use', mapping={
+            0x00: 'SMPTE standard (L, R, C, LFE, Ls, Rs)',
+            0xff: 'Consumer standard (L, R, LFE, C, Ls, Rs)',
+        }),
+        Field('LutSelection', int, 'LUTs', 'Lut Selection', mapping={
+            0x00: 'False',
+            0xff: 'True',
+        }),
+        Field('LutSdiOutEnable', int, 'LUTs', 'SDI Out', mapping={
+            0x00: 'False',
+            0xff: 'True',
+        }),
+        Field('LutHdmiOutEnable', int, 'LUTs', 'HDMI Out', mapping={
+            0x00: 'False',
+            0xff: 'True',
+        }),
+        Field('LutName', str, 'LUTs', 'LUT name'),
+    ]
