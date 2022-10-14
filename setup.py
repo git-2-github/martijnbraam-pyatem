c938a879c906bbb3a810d4a5107a446674e5918e
Martijn Braam <martijn@brixit.nl>
2022-10-14 22:55:43 +0200

    pyatem: update setup.py metadata
    Fix inconsistency in the license designation and have newer python
    releases listed

--- setup.py
+++ setup.py
@@ -12,11 +12,16 @@ setup(
     description='Library implementing the Blackmagic Design Atem switcher protocol',
     long_description=open("README.md").read(),
     classifiers=[
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Operating System :: POSIX :: Linux',
     ],
 )
