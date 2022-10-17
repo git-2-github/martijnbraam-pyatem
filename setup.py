544651621c4125f5527a87042593d735aaa6f3a8
Martijn Braam <martijn@brixit.nl>
2022-10-18 01:40:52 +0200

    Bump version to 0.8.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, Extension
 
 setup(
     name='pyatem',
-    version='0.8.1',
+    version='0.8.2',
     packages=['pyatem', 'pyatem.converters'],
     ext_modules=[Extension('pyatem.mediaconvert', ['pyatem/mediaconvertmodule.c'])],
     url='https://git.sr.ht/~martijnbraam/pyatem',
